5371e81cb1d607c65a047a944e0a77c3cff0bd61
Hamza Khurshid <hamzaandumar@gmail.com>
2019-04-20 22:05:25 +0500

    adding dataSplit file

---
+++ dataSplit.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 17:25:13 2019
+
+@author: hamza
+"""
+
+import os, math
+from shutil import copy
+
+parent="/media/hamza/New Volume/datasets/objects"
+parent2="/media/hamza/New Volume/datasets/objectsSplitted"
+folders=os.listdir(parent)
+#copyfile(src, dst)
+for folder in folders[50:]:
+    files=os.listdir(os.path.join(parent,folder))
+    seventyP=math.ceil(len(files)*0.7)
+    fifteenP=math.ceil(len(files)*0.15)
+    train=files[:seventyP]
+    val=files[seventyP:seventyP+fifteenP]
+    test=files[seventyP+fifteenP:seventyP+2*fifteenP]
+    if not os.path.exists(os.path.join(parent2,"train",folder)):
+        os.makedirs(os.path.join(parent2,"train",folder))
+    if not os.path.exists(os.path.join(parent2,"val",folder)):
+        os.makedirs(os.path.join(parent2,"val",folder))
+    if not os.path.exists(os.path.join(parent2,"test",folder)):
+        os.makedirs(os.path.join(parent2,"test",folder))
+    for file in train:
+        sourcePath=os.path.join(parent,folder,file)
+        destPath=os.path.join(parent2,"train",folder,file)
+        copy(sourcePath,destPath)
+    for file in val:
+        sourcePath=os.path.join(parent,folder,file)
+        destPath=os.path.join(parent2,"val",folder,file)
+        copy(sourcePath,destPath)
+    for file in test:
+        sourcePath=os.path.join(parent,folder,file)
+        destPath=os.path.join(parent2,"test",folder,file)
+        copy(sourcePath,destPath)
+    print(folder)
+    
